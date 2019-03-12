5b550fbe9e775843b057fdf4cc88351ba2680eee
sxiao1 <sxiao1@stuy.edu>
2019-03-12 10:18:42 -0400

    some changes

--- Quick.java
+++ Quick.java
@@ -3,16 +3,39 @@ public class Quick{
   /*return the value that is the kth smallest value of the array.
  */
  public static int quickselect(int []data, int k){
-   Random rand = new Random();
-   int start = Math.abs(rand.nextInt() % data.length);
-   int temp = data[0];
-   data[0] = data[start];
-   data[start] = temp;
-   for(int i = 0; i < data.length;i++){
-     if(k < start){
+   return 1;
+ }
+ public static int qsh(int[] data, int k, int indexb, int indexe){
+   if(indexb == k || indexe == k){
+     return data[k];
+   }
+   else{
+     int middle = data[data.length / 2];
+     int lo = data[0];
+     int hi = data[data.length - 1];
+     if((middle > lo && middle < hi) || (middle < lo && middle > hi)){
+       int temp = data[0];
+       data[0] = middle;
+       data[data.length / 2] = temp;
+       lo = data[0];
+     }
+     for(int i = 0; i < data.length;i++){
+         if(data[i] <= data[0]){
+           int temp2 = data[indexb];
+           data[indexb] = data[i];
+           data[i] = temp2;
+           qsh(data, k, indexb + 1, indexe);
+         }
+         else{
+           if(data[i] >= data[0]){
+             int temp1 = data[indexe];
+             data[indexe] = data[i];
+             data[i] = temp1;
+             qsh(data,k, indexb, indexe + 1);
+           }
+         }
+       }
        return 1;
      }
    }
-   return 1;
  }
-}
