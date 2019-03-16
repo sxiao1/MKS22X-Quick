a24076afb29357187d20515a212ec4a0f07206db
sxiao1 <sxiao1@stuy.edu>
2019-03-16 11:10:39 -0400

    fixed and need to add comments

--- Quick.java
+++ Quick.java
@@ -1,92 +1,94 @@
 import java.util.*;
 import java.io.*;
 public class Quick{
-  /*return the value that is the kth smallest value of the array.
- */
- public static int partition(int[] data, int start,int end){
-   if(end == start){
-     return start;
-   }
-   Random randgen = new Random();
-   //int fstart = data[start];
-   int rand= start + randgen.nextInt((end - start) + 1);
-   int num = data[rand]; //switching the random position to the index 0
-   switchPos(data, start, rand);
-   start= start + 1;
-   int ind = start;
-   while(ind != end){
-     //int r = randgen.nextInt() % 2;
-     if(data[ind] > data[start] ){
-       switchPos(data, ind, end);
-       end--;
-     }
-   else if(data[ind] < data[start]){
-    switchPos(data, ind, start);
-    start ++;
-    ind ++;
-   }
-   else{
-     ind ++;
-   }
- }
- switchPos(data, ind, end);
-   return rand;
- }
- public static void switchPos(int[] data, int num1, int num2){
-   int temp = num1;
-   data[num1] = data[num2];
-   data[num2] = temp;
- }
- public static int quickselect(int []data, int k){
-   int small = 0;
-   int big = data.length - 1;
-   int x = partition(data, small, big);
-   while(x != k){
-     if(x < k){
-       small = x + 1;
-       x = partition(data, small, big);
-     }
-     else{
-       big = x - 1;
-       x = partition(data, small, big);
-     }
-   }
-   return data[x];
- }
- public static void quicksort(int[] data){
-   sortH(data, 0, data.length -1);
- }
- public static void sortH(int[] data, int small, int big){
-   if(big - small <= 20){
-     int ind = small + 1;
-     while(ind <= big){
-       int counter = ind;
-       while(counter > small && data[ind] < data[counter -1]){
-         counter--;
-       }
-       int n = data[ind];
-       for(int i = ind ; i>counter; i--){
-         data[i] = data[i-1];
-       }
-       data[counter] = n;
-         }
-         return;
-   }
-   else{
-   if(small < big){
-     int half = partition(data, small, big);
-     sortH(data, small, half-1);
-     sortH(data, half +1, big);
-   }
- }
- }
- public static String toString(int[] array){
-   String newstr="";
-   for (int i=0;i<array.length;i++){
-    newstr+=array[i]+ ",";
+  public static int partition ( int [] data, int start, int end){
+	   Random randgen = new Random();
+	   int random = start + randgen.nextInt((end - start) + 1);
+	   switchPos(data,start,random);
+	   int lo=start+1;
+	   int high=end;
+	   int index=lo;
+	   while (index <= high){
+      if(data[index] < data[start]){
+        switchPos(data,index,lo);
+        lo++;
+        index++;
+      }
+	    else if(data[index]>data[start]){
+		     switchPos(data,index,high);
+		     high--;
+	    }
+	    else{
+        if(data[index] == data[start]){
+          index++;
+        }
+	    }
+	}
+	   switchPos(data,start,high);
+	   return high;
+	}
+
+  public static void switchPos(int[] data, int num1, int num2){
+	    int temp = data[num2];
+	    data[num2]=data[num1];
+	    data[num1]=temp;
   }
-  return newstr;
+
+  public static int quickselect(int[]data, int k){
+	   int lo=0;
+	   int high= data.length-1;
+	   int x= partition(data,lo,high);
+     while (x !=k){
+	    if (x < k){
+        lo = x + 1;
+		    x = partition(data,lo,high);
+	    }
+	    else{
+		    high = x - 1;
+		    x = partition(data,lo,high);
+	    }
+	   }
+	   return data[x];
   }
+  public static void quicksort(int[] data){
+	   quickH(data,0,data.length-1);
+  }
+  public static void quickH(int[] data,int lo,int high){
+    if (high-lo <=20){
+      sorting(data,lo,high);
+      return;
+    }
+    if(lo < high){
+      int pivot = partition(data, lo, high);
+      quickH(data, lo, pivot - 1);
+      quickH(data, pivot + 1, high);
+    }
+  }
+  public static void sorting(int[] data,int lo,int high){
+	   int index = lo+1;
+	    while(index <= high){
+	       int counter=index;
+	        while(counter > lo && data[index]<data[counter-1]){
+		          counter--;
+	        }
+          shifting(data, index, counter);
+          index++;
+	    }
+  }
+  public static void	shifting(int[] data, int oldNum,int newNum){
+	   int temp=data[oldNum];
+	    for (int i=oldNum;i>newNum;i--){
+	       data[i]=data[i-1];
+	    }
+	    data[newNum]=temp;
+  }
+  public static String toString(int[] data){
+    String newstr="";
+    for (int i=0;i<data.length;i++){
+    newstr+=data[i]+ ",";
+    }
+      return newstr;
+    }
   public static void main(String[]args){
   System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
   int[]MAX_LIST = {1000000000,500,10};
